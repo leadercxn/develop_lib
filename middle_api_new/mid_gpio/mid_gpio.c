8ead68dbe7658d622e161294fc46c2bee5ad5aff
TimChanCHN <874797636@qq.com>
2022-04-09 14:14:20 +0800

    gpio新增复用方式，支持F0/F1芯片

--- middle_api_new/mid_gpio/mid_gpio.c
+++ middle_api_new/mid_gpio/mid_gpio.c
@@ -11,17 +11,28 @@ int gpio_config(gpio_object_t *p_gpio_object)
 
     int err_code = 0;
 
-    if(p_gpio_object->gpio_dir == GPIO_DIR_OUTPUR)
+    if (p_gpio_object->flag == MODE_IO)
     {
-#if defined F0
-        conf_gpio_output(p_gpio_object->gpio_port_periph_clk, p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin);
-#endif
+        if(p_gpio_object->gpio_dir == GPIO_DIR_OUTPUR)
+        {
+    #if defined (FT32) || defined (HK32) || defined (HK32F103)
+            conf_gpio_output(p_gpio_object->gpio_port_periph_clk, p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin);
+    #endif
+        }
+        else
+        {
+    #if defined (FT32) || defined (HK32)
+            conf_gpio_input(p_gpio_object->gpio_port_periph_clk, p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin, GPIO_PuPd_UP);
+    #elif defined (HK32F103)
+            conf_gpio_input(p_gpio_object->gpio_port_periph_clk, p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin);
+    #endif
+        }
     }
-    else
+    else if (p_gpio_object->flag == MODE_AF)
     {
-#if defined F0
-        conf_gpio_input(p_gpio_object->gpio_port_periph_clk, p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin, GPIO_PuPd_UP);
-#endif
+        #if defined (HK32F103)
+        conf_whole_gpios_af(p_gpio_object->gpio_port_periph_clk, p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin);
+        #endif
     }
 
     return err_code;
@@ -36,7 +47,7 @@ int gpio_output_set(gpio_object_t *p_gpio_object, uint8_t value)
 
     int err_code = 0;
 
-#if defined F0
+#if defined (FT32) || defined (HK32) || defined (HK32F103)
     set_gpio_value(p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin, value);
 #endif
 
@@ -52,7 +63,7 @@ int gpio_input_get(gpio_object_t *p_gpio_object, uint8_t *p_value)
 
     int err_code = 0;
 
-#if defined F0
+#if defined (FT32) || defined (HK32) || defined (HK32F103)
     get_gpio_value(p_gpio_object->p_gpio_port, p_gpio_object->gpio_pin, p_value);
 #endif
 
--- middle_api_new/mid_gpio/mid_gpio.h
+++ middle_api_new/mid_gpio/mid_gpio.h
@@ -5,22 +5,32 @@
 #include "f0_lib.h"
 #endif
 
+#ifdef F1
+#include "f1_lib.h"
+#endif
+
 typedef enum
 {
     GPIO_DIR_OUTPUR,
     GPIO_DIR_INPUT,
 } gpio_dir_e;
 
+typedef enum
+{
+    MODE_IO  =   0,
+    MODE_AF  =   1,
+}gpio_mode_e;
+
 typedef struct
 {
-#ifdef F0
+#if (defined F0) || (defined F1)
     uint32_t        gpio_port_periph_clk;
     GPIO_TypeDef    *p_gpio_port;
 #endif
 
     uint32_t    gpio_pin;
     gpio_dir_e  gpio_dir;
-    uint32_t    flag;
+    uint32_t    flag;           // 用作是否复用的标志，0--无复用，1--有复用
 } gpio_object_t;
 
 int gpio_config(gpio_object_t *p_gpio_object);
